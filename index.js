987901ae5e8578ab2d8586c858c64ecf0be96916
sazha13 <sazha@bk.ru>
2016-06-24 00:23:07 +0400

    1

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(process.env.PORT);
+}).listen(process.env.PORT || 8080);
