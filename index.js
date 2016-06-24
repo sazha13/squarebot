eee2c550e25f5f73e68818cd49565a83f4dd518d
sazha13 <sazha@bk.ru>
2016-06-24 22:05:36 +0400

    1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// require('./bot');
-// require('./web');
-var http = require("http");
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(process.env.PORT || 8080);
+require('./bot');
+require('./web');
+// var http = require("http");
+//
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(process.env.PORT || 8080);
