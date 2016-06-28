fc00717846e4f4fdf0fa06a5a11aa13f59820a78
sazha13 <sazha@bk.ru>
2016-06-29 01:41:11 +0400

    use mongodb on localhost

--- bot.js
+++ bot.js
@@ -18,20 +18,51 @@ bot.on('message', function (msg) {
 
     // bot.sendMessage(chatId, answer, { caption: "I'm a bot!" });
     bot1.sendMessage(chatId, answer, { caption: "I'm a bot!" });
-
-});
-bot1.on('message', function (msg) {
-    var chatId = msg.chat.id;
-    console.log(msg);
-    var num = Number(msg.text);
-
-    var answer = "Необходимо вводить числа(лучше положительные). Андрей, тестировщик плохой из тебя. Отрицательные не вводил. ";
-    if (num < 0)
-        answer = "Введите положительное число ";
-    if (num >= 0)
-        answer = Math.sqrt(num).toString();
-
     bot.sendMessage(chatId, answer, { caption: "I'm a bot!" });
-    // bot1.sendMessage(chatId, answer, { caption: "I'm a bot!" });
+
+    Providers.find({ name: 'Alex' }, function(err, exmpl1) {
+      if (err) return console.error(err);
+      // console.dir(exmpl1);
+      bot.sendMessage(chatId, msg.from.username, { caption: "I'm a bot!" });
+    });
+
 
 });
+
+
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+var providersSchema = new mongoose.Schema({
+    name: { type: String }
+    , chatID: Number
+  });
+var Providers = mongoose.model('Providers', providersSchema);
+
+db.on('error', console.error);
+db.once('open', function() {
+  console.log("connection DB ok");
+  // Здесь создаём схемы и модели.
+
+
+    // Компиляция модели Providers с помощью ProvidersSchema в качестве структуры.
+    // Mongoose также создаёт для этих документов коллекцию под названием Providers.
+
+    // var exmpl = new Providers({
+    //   name: 'Alex'
+    //   , chatID: '123444567'
+    // });
+
+    // exmpl.save(function(err, exmpl) {
+    //   if (err) return console.error(err);
+    //   // console.dir(exmpl);
+    // });
+    // console.log("HERE");
+    // Providers.find({ name: 'Alex' }, function(err, exmpl1) {
+    //   if (err) return console.error(err);
+    //   console.dir(exmpl1);
+    // });
+});
+
+mongoose.connect('mongodb://localhost:27017/test');
--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var packageInfo = require('./package.json');
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
 
 var app = express();
 
@@ -21,3 +23,43 @@ var server = app.listen(process.env.PORT||5000, function () {
 
     console.log('Web server started at http://%s:%s', host, port);
 });
+
+// var url = 'mongodb://localhost:27017/test';
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server.");
+//   db.close();
+// });
+// var mongoose = require('mongoose');
+//
+// var db = mongoose.connection;
+//
+// db.on('error', console.error);
+// db.once('open', function() {
+//   console.log("connection ok");
+//   // Здесь создаём схемы и модели.
+//   var providersSchema = new mongoose.Schema({
+//       name: { type: String }
+//       , chatID: Number
+//     });
+//
+//     // Компиляция модели Providers с помощью ProvidersSchema в качестве структуры.
+//     // Mongoose также создаёт для этих документов коллекцию под названием Providers.
+//     var Providers = mongoose.model('Providers', providersSchema);
+//     // var exmpl = new Providers({
+//     //   name: 'Alex'
+//     //   , chatID: '123444567'
+//     // });
+//
+//     // exmpl.save(function(err, exmpl) {
+//     //   if (err) return console.error(err);
+//     //   // console.dir(exmpl);
+//     // });
+//     console.log("HERE");
+//     Providers.find({ name: 'Alex' }, function(err, exmpl1) {
+//       if (err) return console.error(err);
+//       console.dir(exmpl1);
+//     });
+// });
+//
+// mongoose.connect('mongodb://localhost:27017/test');
